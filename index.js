cbab3e91be5c8ac801ab3db083ea7844f3168ed1
River <+>
2022-04-26 11:16:27 -0400

    commit tree 5e15693f170dde5774f9b4c23cb8aadce40e510b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = "surprise"
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return messages;
+}
+
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
